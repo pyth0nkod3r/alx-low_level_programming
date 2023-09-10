2799b669de1b07f2783a6efb3867b4e2e584460b
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-09-10 21:10:25 +0100

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - program start
+* Return: Return 0
+*/
+
+int main(void)
+{
+char x = 97;
+while (x < 122)
+{
+if (x == 101 || x == 113)
+{
+x++;
+continue;
+}
+putchar(x);
+x++;
+}
+putchar(10);
+return (0);
+}
