0e6107aeaac56cd4e6b74caf34ac2a968ed094b5
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-09-11 04:56:43 +0100

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,17 +11,17 @@ int main(void)
 int num_x, num_y;
 for (num_x = 48; num_x <= 57; num_x++)
 {
-for (num_y = 57; num_y >= 48; num_y--)
+for (num_y = num_x+1; num_y <=57; num_y++)
 {
 putchar(num_x);
 putchar(num_y);
-if (num_x != 48 || num_y != 57 )
+if (num_x != 56 || num_y != 57 )
 {
 putchar(44);
 putchar(32);
 }
 }
-putchar(10);
 }
+putchar(10);
 return (0);
 }
