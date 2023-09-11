556ebb3e1fb814d08a76aa6ff7461e40332208aa
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-09-11 05:10:53 +0100

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,17 +8,21 @@
 
 int main(void)
 {
-int num_x, num_y;
+int num_x, num_y, num_z;
 for (num_x = 48; num_x <= 57; num_x++)
 {
 for (num_y = num_x + 1; num_y <= 57; num_y++)
 {
+for (num_z = num_y + 1; num_z <= 57; num_z++)
+{
 putchar(num_x);
 putchar(num_y);
-if (num_x != 56 || num_y != 57)
+putchar(num_z);
+if (num_x != 55 || num_y != num_x + 1 || num_z != num_y + 1)
 {
 putchar(44);
 putchar(32);
+}
 }
 }
 }
