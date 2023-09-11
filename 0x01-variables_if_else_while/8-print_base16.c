e2e73aa68a41d1202b6a4e0b5e585bc9cd7af556
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-09-11 03:13:40 +0100

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - program start
+* Return: Zero
+*/
+
+
+int main(void)
+{
+char x, y;
+for (x = 48; x < 58 ; x++)
+{
+putchar(x);
+}
+for (y = 97; y < 103; y++)
+{
+putchar(y);
+}
+putchar(10);
+return (0);
+}
