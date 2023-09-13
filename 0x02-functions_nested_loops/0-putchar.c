e15e8c085616e5e112f48d23195961556ec6d0e3
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-09-13 16:37:38 +0100

    C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 /**
 * main - program start
@@ -5,6 +6,14 @@
 */
 int main(void)
 {
-printf("_putchar\n");
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
 return (0);
 }
