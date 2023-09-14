d7d2b359f1f0be1dcf9682c9463eaeef6ecb42d7
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-09-14 20:34:50 +0100

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/3-is_lower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - checks for lower case
+ * Return: 1 if lowercaser otherwise, 
+ */
+
+int _islower(int c)
+{
+c = 96;
+if (c > 97 && c < 123)
+{
+return (1);
+}   
+return (0); 
+}
+
