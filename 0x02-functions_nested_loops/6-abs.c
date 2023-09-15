c09472e7d7e03286b67c7d3bd2608c88868f283f
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-09-15 03:03:29 +0100

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _abs - writes absolute int val
+* @int: int type input
+* Return: type int
+*/
+
+int _abs(int)
+{
+int i;
+if (i > 1)
+{
+return (i);
+}
+else if (i == 0)
+{
+return (-i);
+}
+return (0);
+}
