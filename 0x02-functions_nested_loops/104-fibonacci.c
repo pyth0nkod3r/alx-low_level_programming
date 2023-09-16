d66f992c628467fb00806ea7c933c25bfa8f5664
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-09-16 18:21:04 +0100

    C-0x02-functions_nested_loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,41 +1,46 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+#define LARGEST 10000000000
 
 /**
- * fibonacci - fibonacci
- * @n: input
- */
-
-void fibonacci(int n)
-{
-	unsigned long int i, first = 1, second = 2, next;
-
-	printf("%lu, %lu, ", first, second);
-
-	for (i = 3; i <= n; i++)
-	{
-		next = first + second;
-
-		printf("%lu", next);
-
-		if (i != n)
-		{
-			printf(", ");
-		}
-
-		first = second;
-		second = next;
-	} printf("\n");
-}
-
-
-/**
- * main - function
+ * main - entry point
+ * Description: Find and print the first 98 fibonacci numbers
  * Return: 0
  */
 int main(void)
 {
-	unsigned long int n = 98;
+	/* Divide the number into two parts and create holders for them */
+	int i;
+	uint64_t fpart_num1 = 0, spart_num1 = 1;
+	uint64_t fpart_num2 = 0, spart_num2 = 2;
+	uint64_t num_holder1, num_holder2, num_holder3;
 
-	fibonacci(n);
+	printf("%" PRId64 ", ", spart_num1);
+	printf("%" PRId64 ", ", spart_num2);
+
+	for (i = 3; i <= 98; i++)
+	{
+		if (spart_num1 + spart_num2 > LARGEST || fpart_num1 > 0 || fpart_num2 > 0)
+		{
+			num_holder1 = (spart_num1 + spart_num2) / LARGEST;
+			num_holder2 = (spart_num1 + spart_num2) % LARGEST;
+			num_holder3 = fpart_num1 + fpart_num2 + num_holder1;
+			fpart_num1 = fpart_num2, fpart_num2 = num_holder3;
+			spart_num1 = spart_num2, spart_num2 = num_holder2;
+			printf("%lu%010lu", fpart_num2, spart_num2);
+		}
+		else
+		{
+			num_holder2 = (spart_num1 + spart_num2);
+			spart_num1 = spart_num2, spart_num2 = num_holder2;
+			printf("%" PRId64 "", spart_num2);
+		}
+		if (i != 98)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
 	return (0);
 }
