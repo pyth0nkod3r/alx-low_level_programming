eb7aeb46912a0b6b04d7b167696defb2ce2c1c8f
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-09-16 10:54:28 +0100

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @num1: first parameter
+ * @num2: second parameter
+ * Result: sum of the two porameters
+*/
+
+int add(int num1, int num2)
+{
+int sum = num1 + num2;
+return (sum);
+}
