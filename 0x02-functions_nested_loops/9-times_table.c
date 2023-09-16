a888ad1c11f8ebe1ee5cba2d1a07e2444876123f
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-09-16 10:43:05 +0100

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,26 +1,38 @@
-#include "main.h"
+#include "main.h" 
 /**
  * times_table - prints times table
  * Return: void
 */
 void times_table(void)
 {
-int num1, num2, ans;
-for(num1 = 0; num1 < 10; num1++)
+int num1, num2, ans, count;
+count = 10;
+
+for(num1 = 0; num1 < count; num1++)
 {
-_putchar('0');
-for(num2 = 0; num2 < 10; num2++)
+for(num2 = 0; num2 < count; num2++)
 {
-    _putchar(',');
-    _putchar(' ');
 ans = num1 * num2;
-if (ans < 10)
+if(num2 == 0)
 {
-_putchar(' ');
+_putchar(0 + '0');
+
 }
-_putchar((ans / 10) + '0' );
+if(ans < count && num2 != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(ans + '0');
+}
+else if (ans >= count)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((ans / 10) + '0');
 _putchar((ans % 10) + '0');
 }
-_putchar('\n');
+}
+_putchar(10);
 }
 }
