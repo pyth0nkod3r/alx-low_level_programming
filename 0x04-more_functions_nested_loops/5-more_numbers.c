af1d7203035a13b69aa3401d4a321f004a96f294
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-09-18 15:15:49 +0100

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - prints (0-14)x10
+ * Return: 0-14 x10
+*/
+
+void more_numbers(void)
+{
+	int i;
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9)
+			{
+				_putchar((i / 10 + '0'));
+			}
+			
+			_putchar((i % 10) + '0');
+		}
+		_putchar(10);
+	}
+
+}
