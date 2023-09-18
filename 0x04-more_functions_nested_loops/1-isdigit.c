11cf45fb75812463d4c87d983a7163b73ed52618
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-09-18 14:05:06 +0100

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,7 +7,7 @@
 
 int _isdigit(int c)
 {
-	if ((c >= 47) && (c <= 53))
+	if ((c >= 48) && (c <= 57))
 	{
 		return (1);
 	}
--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -10,7 +10,7 @@ int main(void)
 {
     char c;
 
-    c = '0';
+    c = 'A';
     printf("%c: %d\n", c, _isdigit(c));
     c = '1';
     printf("%c: %d\n", c, _isdigit(c));
