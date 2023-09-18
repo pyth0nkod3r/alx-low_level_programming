758c15ee3bb717106c683175a9322417e1cadf65
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-09-18 14:22:11 +0100

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,6 +1,14 @@
+/**
+ * mul - multiplies two ints
+ * @a : param 1
+ * @b: param 2
+ * Return: answer to multiplication
+ */
+
 int mul(int a, int b)
 {
 	int result;
+
 	result = a * b;
 	return (result);
 }
