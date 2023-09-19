2460fcad53f2be0cfb59e9aa5dcd7b2d1121fade
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-09-19 16:44:46 +0100

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - prints square
+ * @size: parameter
+*/
+
+void print_square(int size)
+{
+	int count, mul;
+
+	count = 0;
+	if (count < 0)
+	{
+		_putchar('\n');
+	}
+	else if (size > count)
+	{
+		for (count = 0; count < size; count++)
+		{
+			for (mul = 0; mul < size; mul++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+	}
+}
+}
