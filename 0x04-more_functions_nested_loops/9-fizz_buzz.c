0b236e27cd9284ef72c321f70e5def841f05b2d8
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-09-19 17:38:38 +0100

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,43 +1,37 @@
 #include <stdio.h>
 
 /**
-*main - program that prints either number
-*or fizz or buzz or fizzBuzz
-*
-*Return: returns 0
-*/
-
+ * main - entry
+ * Return: 0
+ *
+ */
 int main(void)
 {
-	int num = 1;
+	int i;
 
-	while (num++ < 100)
+	for (i = 1; i <= 100; i++)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
+		if (i % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
-		else if ((num % 3) == 0)
+		else if (i % 5 == 0)
 		{
-			printf("Fizz ");
+			printf(" Buzz");
 		}
-		else if ((num % 5) == 0)
+		else if (i % 3 == 0)
 		{
-			if (num != 100)
-			{
-				printf("Buzz ");
-			}
-			else
-			{
-				printf("Buzz");
-			}
+			printf(" Fizz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
 		}
 		else
 		{
-			printf("%d ", num);
+			printf(" %d", i);
 		}
 	}
 	printf("\n");
-
 	return (0);
 }
