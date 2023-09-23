0e70a45edbd50e5946267bc79e3de74baef3bcb7
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-09-23 18:38:32 +0100

    C-0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: takes pointer of char type as input
+ * Return: Integer
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
