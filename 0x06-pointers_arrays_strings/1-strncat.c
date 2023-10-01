74f8d39e7bab583a67534c0594c5f09bb9aebddd
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-10-01 01:16:22 +0100

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * strncat - concatenates two strings
+ * @dest: destination parameter
+ * @src: source parameter
+ * @n: number of bytes
+ * 
+ * Return: pointer to @dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len, count;
+
+	dest_len = _strlen(dest);
+	for (count = 0; count < n; count++)
+	{
+		dest[dest_len + count] = src[count];
+		if (dest[dest_len + count] == '\n')
+		{
+			break;
+		}
+		
+	}
+	return (dest);
+}
