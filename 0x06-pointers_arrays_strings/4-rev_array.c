8e4681f04bedd851536418ab63b3c3b676125aea
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-10-01 11:03:22 +0100

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * reverse_array - reverses the content of an array
+ * @a: first parameter
+ * @n: second parameter
+ * 
+*/
+void reverse_array(int *a, int n)
+{
+	int i = 0, tmp;
+
+	while (i < (n / 2))
+	{
+		tmp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = tmp;
+		i++;
+	}
+}
