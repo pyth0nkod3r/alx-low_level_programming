2a465c272e3e549331942f8407da551e351b5dc4
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-10-01 18:56:10 +0100

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,51 +1,53 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <string.h>
 /**
  * infinite_add - adds two numbers
  * @n1: num 1
  * @n2: num 2
  * @r: result buffer
  * @size_r: buffer size
- *
  * Return: r or 0
 */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, sum, carry;
+	int len1 = strlen(n1);
+	int len2 = strlen(n2);
+	int i, j, k, carry, left, right;
 
-	for (i = 0; n1[i]; i++)
-	;
-	for (j = 0; n2[j]; j++)
-	;
-	i--;
-	j--;
-	sum = (n1[i] - '0') + (n2[j] - '0');
-	if (sum > 9)
+	/* Check if the result can fit in the buffer */
+	if (len1 + len2 >= size_r)
+		return (0);
+	i = len1 - 1;
+	j = len2 - 1;
+	k = 0;
+	carry = 0;
+	while (i >= 0 || j >= 0 || carry)
 	{
-		carry = 1;
-		sum -= 10;
-	}
-	else
-		carry = 0;
-	r[--size_r] = sum + '0';
-	while (i >= 0 || j >= 0)
-	{
+		int sum = carry;
+
 		if (i >= 0)
-		{
-			sum = (n1[i--] - '0') + (carry);
-			carry = (sum > 9) ? 1 : 0;
-			sum = sum % 10;
-		}
+			sum += n1[i] - '0';
 		if (j >= 0)
-		{
-			sum += n2[j--] - '0';
-			carry = (sum > 9) ? 1 : 0;
-			sum = sum % 10;
-		}
-		r[--size_r] = sum + '0';
+			sum += n2[j] - '0';
+		r[k++] = (sum % 10) + '0';
+		carry = sum / 10;
+		i--;
+		j--;
 	}
-	if (carry)
-		r[--size_r] = '1';
-	return (r + size_r);
+	r[k] = '\0';
+	/* Reverse the result string */
+	left = 0;
+	right = k - 1;
+	while (left < right)
+	{
+		char temp = r[left];
+
+		r[left] = r[right];
+		r[right] = temp;
+		left++;
+		right--;
+	}
+	return (r);
 }
