742d50a73e9b342123c5d4a01592204642890d89
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-10-02 04:01:10 +0100

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,11 +11,11 @@ void reverse_array(int *a, int n)
 {
 	int i = 0, tmp;
 
-	while (i < (n / 2))
+	while (i < n--)
 	{
 		tmp = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = tmp;
+		a[i] = a[n];
+		a[n] = tmp;
 		i++;
 	}
 }
