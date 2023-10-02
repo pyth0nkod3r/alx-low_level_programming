195dc6bfedea984478fde9a8f917b77691ed3fc3
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-10-02 04:37:02 +0100

    0x08. C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints's a string followed by a new line
- * @s: string to be printed
- *
+ * _puts_recursion - prints a string followed by a new line
+ * @s: character to print
  * Return: void
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	int i = 0;
+
+	if (s[i] == '\0')
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		_putchar(s[0]);
-		_puts_recursion(s + 1);
+		_putchar(s[i]);
+		i++;
+		_puts_recursion(s + i);
 	}
 }
