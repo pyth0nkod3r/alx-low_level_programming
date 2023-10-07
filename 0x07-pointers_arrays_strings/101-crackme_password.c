8c8d094e93e6c2eea09393a8ad370d5e79a04a70
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-10-07 12:13:50 +0100

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/101-crackme_password.c
+++ 0x07-pointers_arrays_strings/101-crackme_password.c
@@ -1,33 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define PASSWORD_LENGTH 10
-const char password[PASSWORD_LENGTH] = "abc123";
-
-void writePasswordToFile();
 
 int main()
 {
-	writePasswordToFile();
-	return (0);
-}
-
-
-void writePasswordToFile()
-{
-	int i;
-
-	FILE *file = fopen("crackme2", "wb");
-	if (file == NULL)
-	{
-		printf("Error. Exting..\n");
-		exit(1);
-	}
-	for (i = 0; i < PASSWORD_LENGTH; i++)
-	{
-		fputc(password[i], file);
-	}
+	char password[] = "abc123";
 	
+	FILE *file = fopen("101-crackme_password", "w");
+	fprintf(file, "%s", password);
 	fclose(file);
+	return (0);
 }
