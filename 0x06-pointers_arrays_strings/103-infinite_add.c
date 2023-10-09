9f664a30fce8925949d10f805a1d00b346f71a9f
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-10-09 20:06:48 +0100

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,53 +1,78 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
+
 /**
- * infinite_add - adds two numbers
- * @n1: num 1
- * @n2: num 2
- * @r: result buffer
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
  * @size_r: buffer size
- * Return: r or 0
-*/
+ * Return: pointer to calling function
+ */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = strlen(n1);
-	int len2 = strlen(n2);
-	int i, j, k, carry, left, right;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	/* Check if the result can fit in the buffer */
-	if (len1 + len2 >= size_r)
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
 		return (0);
-	i = len1 - 1;
-	j = len2 - 1;
-	k = 0;
-	carry = 0;
-	while (i >= 0 || j >= 0 || carry)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		int sum = carry;
-
-		if (i >= 0)
-			sum += n1[i] - '0';
-		if (j >= 0)
-			sum += n2[j] - '0';
-		r[k++] = (sum % 10) + '0';
-		carry = sum / 10;
-		i--;
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
 		j--;
+		i--;
 	}
-	r[k] = '\0';
-	/* Reverse the result string */
-	left = 0;
-	right = k - 1;
-	while (left < right)
-	{
-		char temp = r[left];
-
-		r[left] = r[right];
-		r[right] = temp;
-		left++;
-		right--;
-	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
 	return (r);
 }
