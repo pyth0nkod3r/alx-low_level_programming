b7e47da64073275a137dc31f2c8badc338796cf9
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-10-09 20:11:17 +0100

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,49 +1,50 @@
 #include "main.h"
 #include <stdio.h>
-#include <ctype.h>
+
 /**
-*print_buffer -  prints buffer
-*@b: number of bytes
-*@size: size of the byte
-*/
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
+ */
+
 void print_buffer(char *b, int size)
-{ int i, j;
+{
+	int o, j, i;
+
+	o = 0;
+
 	if (size <= 0)
 	{
 		printf("\n");
 		return;
 	}
-	for (i = 0; i < size; i += 10)
+	while (o < size)
 	{
-		printf("%08x: ", i);
-		for (j = 0; j < 10; j++)
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
 		{
-			if (i + j < size)
-			{
-				printf("%02x", b[i + j]);
-				if (j % 2 != 0)
-				{
-					printf(" ");
-				}
-			} else
-			{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
 				printf("  ");
-				if (j % 2 != 0)
-				{
-					printf(" ");
-				}
-			}
-		} printf(" ");
-		for (j = 0; j < 10; j++)
-		{
-			if (i + j < size)
+			if (i % 2)
 			{
-				char c = isprint(b[i + j]) ? b[i + j] : '.';
-
-				printf("%c", c);
-			} else
-			{printf(" ");
+				printf(" ");
 			}
 		}
-		printf("\n"); }
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+	}
 }
