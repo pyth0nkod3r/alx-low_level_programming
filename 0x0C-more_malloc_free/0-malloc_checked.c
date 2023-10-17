8a91ea674a84db4e46d3ef1421d40b1251ac5b4b
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2023-10-17 05:35:57 +0100

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,7 +4,8 @@
  * malloc_checked - allocates memory
  * @b: amount of bytes
  *
- * Return: pointer to the allocated memory * if malloc fails, status value is equal to 98
+ * Return: pointer to the allocated memory
+ * if malloc fails, status value is equal to 98
  */
 
 void *malloc_checked(unsigned int b)
